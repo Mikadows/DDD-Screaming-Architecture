62282f220650c35122a9dc6b9e96573066d1ce35
Gwendal <gwendou95@hotmail.fr>
2022-03-16 15:35:12 +0100

    Regles gestion

---
+++ src/main/java/use_cases/ValiderCommande.java
@@ -0,0 +1,13 @@
+package use_cases;
+
+public class ValiderCommande {
+
+    /**
+     * Règles de gestion
+     *
+     * 1. Le client doit avoir accepter les CGV
+     * 2. Le client doit avoir renseigner son adresse de livraison
+     * 3. Le client doit avoir renseigner son moyen de paiement
+     * 4. La commande est validé
+     */
+}
